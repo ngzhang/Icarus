d3514578c15e68cd2b29a049ae9255b3093025cd
ngzhang <ngzhang1983@msn.com>
2011-12-04 16:20:40 +0800

    Signed-off-by: ngzhang <ngzhang1983@msn.com>

--- miner_software/mine.tcl
+++ miner_software/mine.tcl
@@ -1,29 +1,22 @@
 ##
+#   Copyright 2011 ngzhang & lee
+#   ngzhang1983@msn.com
 #
-# Copyright (c) 2011 fpgaminer@bitcoin-mining.com
+#   This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License version 3 as
+#   published by the Free Software Foundation.
 #
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#   General Public License for more details.
 #
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, see http://www.gnu.org/licenses/.
+#  
+#   This program is originate from fpgaminer@bitcoin-mining.com 
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
 ##
-
-
-## TODO: Long polling.
-## TODO: --verbose option for debugging issues.
-## TODO: Handle multiple FPGAs at once.
-
 
 package require http
 package require json
