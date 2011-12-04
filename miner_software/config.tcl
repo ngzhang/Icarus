fd87953da39e34eade0e0a478d23f334f299fd47
ngzhang <ngzhang1983@msn.com>
2011-12-04 16:12:24 +0800

    Signed-off-by: ngzhang <ngzhang1983@msn.com>

---
+++ miner_software/config.tcl
@@ -0,0 +1,21 @@
+# Edit this with your details and save as config.tcl
+# They are the same worker server, user/pass you use
+# with other mining software.
+#
+#set your user/pass here:
+#default is mining for me as a donate, so don't forget to mod it! :D
+set url "http://pit.deepbit.net:8332"
+set userpass "ngzhang1983@msn.com_10:1234"
+
+#set the com port number here:
+set com_number 7
+
+#get work every ms:
+set time_slice 1500
+
+#miner time_out: larger than 600 is ok.
+set time_out 6000
+
+#set miner nonce to number.
+set miner_to "7ffffffe"
+
---
+++ miner_software/minner.bat
@@ -0,0 +1,3 @@
+tclsh mine.tcl
+
+PAUSE
