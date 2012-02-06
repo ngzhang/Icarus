becdca420678a2a446cdee8c89968179dfe9a75c
Xiangfu Liu <xiangfu.z@gmail.com>
2012-02-06 12:36:37 +0800

    merge icarus_restart and icarus_monitor to one

--- miner_software/scripts/icarus_monitor.sh
+++ miner_software/scripts/icarus_monitor.sh
@@ -7,5 +7,10 @@ ${SCRIPT_PATH}/icarus_undermanager.py > ${SCRIPT_PATH}/u.log 2>&1
 TRUE_COUNT=`more ${SCRIPT_PATH}/u.log | grep "\"alive\": true" | wc -l`
 if [ "${TRUE_COUNT}" == "0" ]; then
     echo `date`  >> ${SCRIPT_PATH}/restart.log
-    ${SCRIPT_PATH}/icarus_restart_miner.sh
+
+    killall -s 15 miner.sh
+    ps ax | grep "python ./miner.py" | grep -v grep | sed 's/^ *//' | cut -d ' ' -f 1 | xargs kill -15
+
+    ICARUS_MINING_PATH="/home/xiangfu/PanGu/Icarus/Icarus-Mining/miner0/queue"
+    (cd ${ICARUS_MINING_PATH} && ./miner.sh &)
 fi
--- miner_software/scripts/icarus_restart_miner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-killall -s 15 miner.sh
-ps ax | grep "python ./miner.py" | grep -v grep | sed 's/^ *//' | cut -d ' ' -f 1 | xargs kill -15
-
-ICARUS_MINING_PATH="/home/xiangfu/PanGu/Icarus/Icarus-Mining"
-(cd ${ICARUS_MINING_PATH}/miner0/queue && ./miner.sh &)
