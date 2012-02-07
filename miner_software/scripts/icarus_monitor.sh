185695e57f4d5abfa17998fd7e82e8cb1bcf505f
xiangfu <xiangfu@openmobilefree.net>
2012-02-06 22:37:07 -0800

    when hashrate == 0 or true_count = 0 restart miner

--- miner_software/scripts/icarus_monitor.sh
+++ miner_software/scripts/icarus_monitor.sh
@@ -4,8 +4,10 @@ SCRIPT_PATH=`pwd`
 
 ${SCRIPT_PATH}/icarus_undermanager.py > ${SCRIPT_PATH}/u.log 2>&1
 
-TRUE_COUNT=`more ${SCRIPT_PATH}/u.log | grep "\"alive\": true" | wc -l`
-if [ "${TRUE_COUNT}" == "0" ]; then
+TRUE_COUNT=`more ${SCRIPT_PATH}/u.log | grep "\"alive\": true"   | wc -l`
+HASHRATE=`more ${SCRIPT_PATH}/u.log | grep "\"hashrate\": 0,"  | wc -l`
+
+if [ "${TRUE_COUNT}" == "0" ] || [ "${HASHRATE}" == "1" ]; then
     echo `date`  >> ${SCRIPT_PATH}/restart.log
 
     killall -s 15 miner.sh
